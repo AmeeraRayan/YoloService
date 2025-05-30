9c99aacab9c5bc4c71a1a268da4694676d621108
vboxuser <ameerarayan2@gmail.com>
2025-05-30 09:26:38 +0000

    update monitoring

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,42 @@
 #!/bin/bash
+
+if ! command -v otelcol &> /dev/null; then
+  echo "📡 Installing OpenTelemetry Collector..."
+  sudo yum update
+  sudo yum -y install wget systemctl
+  wget https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/v0.127.0/otelcol_0.127.0_linux_amd64.rpm
+  sudo rpm -ivh otelcol_0.127.0_linux_amd64.rpm
+else
+  echo "✅ OpenTelemetry Collector already installed, skipping install."
+fi
+
+echo "⚙️ Writing OpenTelemetry Collector config to /etc/otelcol/config.yaml..."
+sudo tee /etc/otelcol/config.yaml > /dev/null <<EOL
+receivers:
+  hostmetrics:
+    collection_interval: 15s
+    scrapers:
+      cpu:
+      memory:
+      disk:
+      filesystem:
+      load:
+      network:
+      processes:
+
+exporters:
+  prometheus:
+    endpoint: "0.0.0.0:8889"
+
+service:
+  pipelines:
+    metrics:
+      receivers: [hostmetrics]
+      exporters: [prometheus]
+EOL
+
+sudo systemctl restart otelcol
+
 # copy the .servcie file
 sudo cp yolo.service /etc/systemd/system/
 # reload daemon and restart the service
